5e5fc3dbcb0c79ba3bf2c72015f24a18c1b20b3f
jhoel00852 <128112657+Jhoel00852@users.noreply.github.com>
2023-07-17 18:01:54 -0500

    second

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,8 +6,8 @@ database:process.env.DB_DATABASE,
 port: process.env.DB_PORT,
 username: process.env.DB_USERNAME,
 password:process.env.DB_PASSWORD,
-dialect: 'postgres'
-// dialectOptions: {ssl: { require: true, rejectUnauthorized: false }}
+dialect: 'postgres',
+dialectOptions: {ssl: { require: true, rejectUnauthorized: false }}
 })
 
 module.exports = db;
